659cbfffd03d486e9c5552218a0976cc356ec523
deeppatel26075 <deeppatel26075@gmail.com>
2026-09-14 10:31:52 +0530

    chore(server): update server startup comment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = require('./app');
 const connectDB = require('./config/database');
 
 const PORT = process.env.PORT || 5000;
+// Trigger restart for MongoDB Atlas
 
 const startServer = async () => {
   // Initialize database connection
